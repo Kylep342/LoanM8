311425b7acf0f7beab6f6663001af4aae7dccbf2
Kylep342 <kylep342@gmail.com>
2018-09-01 09:05:26 -0500

    Removing bogus colors

--- assets/js/graphColor.js
+++ assets/js/graphColor.js
@@ -1,36 +1,18 @@
 function graphColor(index) {
-  var digit = index % 10;
+  var digit = index % 4;
 
   switch (digit) {
       case 0:
         color = '#2847d3';
         break;
       case 1:
-        color = '#f29a1f';
+        color = '#2ff2aa';
         break;
       case 2:
-        color = '#1ff270';
-        break;
-      case 3:
         color = '#f21f49';
         break;
-      case 4:
+      case 3:
         color = '#a229ce';
-        break;
-      case 5:
-        color = '#f4ec53';
-        break;
-      case 6:
-        color = '#999999';
-        break;
-      case 7:
-        color = '#4cdbe8';
-        break;
-      case 8:
-        color = '#db3082';
-        break;
-      case 9:
-        color = '#0f7519';
         break;
   };
   return color;
