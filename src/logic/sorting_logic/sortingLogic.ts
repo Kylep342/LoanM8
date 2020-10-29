5d87c13c782bdfa290c65b6e83d402c57ede02fa
Kylep342 <kylep342@gmail.com>
2020-10-28 23:23:32 -0500

    Adds more documentation around the sort function Changes 'sort' variable, to 'avalanche', so as to identify sorting method versus falesly indicating whether or not the loans were sorted

--- src/logic/sorting_logic/sortingLogic.ts
+++ src/logic/sorting_logic/sortingLogic.ts
@@ -1,9 +1,13 @@
 import { Loan } from "../../Loan";
 
-export const sortLoans = (loans: Array<Loan>, sort: boolean = true) => {
-    // sort will be implemented to indicate whether the repayment method is
+/*
+    sortLoans is used to order loans for priority
+    determinging the order in which loans receive excess payment
+*/
+export const sortLoans = (loans: Array<Loan>, avalanche: boolean = true) => {
+    // avalanche is implemented to indicate whether the repayment method is
     // snowball (false) or avalanche (true)
-    if (sort) {
+    if (avalanche) {
         loans.sort(function (loan1, loan2) {
             return loan2.interestRate - loan1.interestRate
         })
