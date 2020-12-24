27dd797b6b15af306d1f78665a735524bca2a7b6
Kylep342 <kylep342@gmail.com>
2020-12-23 23:32:12 -0600

    Cleans up the header a bit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,16 @@ import "./style.css";
 const Header: React.FunctionComponent = () => {
     return (
         <header className="header">
-            <h1 className="header-title">LoanM8</h1>
-            <a href="https://www.github.com/kylep342/LoanM8" target="_blank">
-                <i className="fab fa-github fa-lg white" aria-hidden="true"></i>
-            </a>
+            <div className="header-content align-left">
+                <h1 className="header-title">LoanM8</h1>
+            </div>
+            <div className="header-content align-right">
+                <a className="header-link" href="https://www.github.com/kylep342/LoanM8" target="_blank">
+                    <i className="fab fa-github fa-lg white" aria-hidden="true"></i>
+                </a>
+            </div>
         </header>
     )
 }
 
 export default Header;
-``
